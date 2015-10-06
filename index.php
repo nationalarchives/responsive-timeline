7c0cec9335b3d00b77694f4e47db9b0e32b08d1c
Chris Bishop <domingobishop@gmail.com>
2015-10-06 10:04:42 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en-gb">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Responsive timeline</title>
+	<link rel="stylesheet" type="text/css" href="css/responsive-timeline.css">
+	<link href='http://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+	<meta content="initial-scale = 1.0" name="viewport">
+</head>
+<body>
+<section class="tna-business-plan">
+    <div class="bp-backBlue">
+        <div class="container">
+            <div class="row">
+                <div class="col starts-at-full ends-at-full">
+                    <div class="bp-timeline">
+                        <div class="breather">
+                            <ul>
+                                <li class="tl-item">
+                                    <div class=“tl-year”><h4>2015</h4></div>
+                                    <div class="tl-thumb pipe-above"><img src="<?php echo get_template_directory_uri(); ?>/images/business-plan/2015.jpg" /></div>
+                                    <div class="tl-text">
+                                        <p>Mark Magna Carta 800th anniversary commemorations.</p>
+                                    </div>
+                                </li>
+                                <li class="tl-item">
+                                    <div class="tl-year"><h4>2016</h4></div>
+                                    <div class="tl-thumb pipe-below tl-red"><img src="<?php echo get_template_directory_uri(); ?>/images/business-plan/2016.jpg" /></div>
+                                    <div class="tl-text">
+                                        <p>Shakespeare 400: new research using our unique Shakespeare documents will provide insight into his life.</p>
+                                    </div>
+                                </li>
+                                <li class="tl-item">
+                                    <div class="tl-year"><h4>2017</h4></div>
+                                    <div class="tl-thumb pipe-above tl-teal"><img src="<?php echo get_template_directory_uri(); ?>/images/business-plan/2017.jpg" /></div>
+                                    <div class="tl-text">
+                                        <p>Supporting the archive sector by establishing a sector wide strategic framework.</p>
+                                    </div>
+                                </li>
+                                <li class="tl-item">
+                                    <div class="tl-year"><h4>2018</h4></div>
+                                    <div class="tl-thumb pipe-below"><img src="<?php echo get_template_directory_uri(); ?>/images/business-plan/2018.jpg" /></div>
+                                    <div class="tl-text">
+                                        <p>Create a greater public understanding of archives’ value and significance in modern times.</p>
+                                    </div>
+                                </li>
+                                <li class="tl-item">
+                                    <div class="tl-year"><h4>2019</h4></div>
+                                    <div class="tl-thumb pipe-above tl-red"><img src="<?php echo get_template_directory_uri(); ?>/images/business-plan/2019.jpg" /></div>
+                                    <div class="tl-text">
+                                        <p>Culmination of First World War 100 centenary programme.</p>
+                                    </div>
+                                </li>
+                                <div class="tl-line"></div>
+                                <div class="clr"></div>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
